cb848794c384dbea596c06cb016ee9a67b2dae9a
ir16rogi <118218265+ir16rogi@users.noreply.github.com>
2023-02-05 10:25:27 +0100

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -2,7 +2,7 @@ simplyCountdown('#cuenta', {
 	year: 2023, // required
 	month: 2, // required
 	day: 6, // required
-	hours: 20, // Default is 0 [0-23] integer
+	hours: 14, // Default is 0 [0-23] integer
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
